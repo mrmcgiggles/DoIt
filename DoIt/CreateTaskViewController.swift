b3e2a2bc01ec9609df55eaea665a0f121ece0d04
AJ Lodge <ahhjlodge@gmail.com>
2017-03-22 12:49:38 -0700

    Just added switch, button on CreateTaskViewController

---
+++ DoIt/CreateTaskViewController.swift
@@ -0,0 +1,27 @@
+//
+//  CreateTaskViewController.swift
+//  DoIt
+//
+//  Created by AJ Lodge on 3/22/17.
+//  Copyright © 2017 AJ Lodge. All rights reserved.
+//
+
+import UIKit
+
+class CreateTaskViewController: UIViewController {
+
+    @IBOutlet weak var taskNameTextField: UITextField!
+    
+    @IBOutlet weak var importantSwitch: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func addTapped(_ sender: Any) {
+        
+    }
+   
+}
---
+++ DoIt/task.swift
@@ -0,0 +1,14 @@
+//
+//  task.swift
+//  DoIt
+//
+//  Created by AJ Lodge on 3/22/17.
+//  Copyright © 2017 AJ Lodge. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
